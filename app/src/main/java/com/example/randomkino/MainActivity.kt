cf99c97b4988b2190b5dff1532fe2fc987579f54
Varushka ☆彡 <fedorova04_varya@mail.ru>
2024-03-12 22:41:14 +0300

    Authorization page

--- app/src/main/java/com/example/randomkino/MainActivity.kt
+++ app/src/main/java/com/example/randomkino/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.randomkino
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_page)
     }
+
+//    fun RegPage(view: View){
+//        val RegIntent = Intent(this, RegistrationActivity::class.java)
+//        startActivity(RegIntent)
+//    }
 }
